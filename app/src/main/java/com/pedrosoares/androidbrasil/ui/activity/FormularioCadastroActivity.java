6da88d85d5c223d8973a20dd3481ab5e9387eac5
Pedro Soares <pedrinhocef@gmail.com>
2018-06-11 22:03:32 -0300

    criando a activity_formulario usando o ConstraintLayout e TextInputLayout,

---
+++ app/src/main/java/com/pedrosoares/androidbrasil/ui/activity/FormularioCadastroActivity.java
@@ -0,0 +1,16 @@
+package com.pedrosoares.androidbrasil.ui.activity;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pedrosoares.androidbrasil.R;
+
+public class FormularioCadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_cadastro);
+    }
+}
